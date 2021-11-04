4191e8aec864d28d308891fa18cf33e380970ec8
Eelin <xyl-eelin@qq.com>
2021-11-04 23:11:30 +0800

    1104
    发现问题

--- track.py
+++ track.py
@@ -12,11 +12,11 @@ for root, dirs, files in os.walk('D:/Workspace/VOT2019-rgbtir'):
 
 def track(tracker_model, tracker_name):
     miss = 0
-    accuracy = 0
+    # accuracy = 0
 
     for target in filelist:
-        # if target != 'car20':
-        #     continue
+        if target != 'car10':
+            continue
 
         path = 'D:/Workspace/VOT2019-rgbtir/' + target
         img_path = '/'.join([path, 'color'])
@@ -32,6 +32,7 @@ def track(tracker_model, tracker_name):
             # print(data)
 
         for idx in range(len(img_list)):
+            print(idx)
 
             img = os.path.join(img_path, img_list[idx])
             GT_val = GT_val_list[idx].split(',')
@@ -60,13 +61,13 @@ def track(tracker_model, tracker_name):
 
                     # trager = do_sth
 
-                    box1 = (min(int(box[0]), int(box[0] + box[2])), min(int(box[1]), int(box[1] + box[3])))
-                    gt1 = (min(GT_val[0], GT_val[4]), min(GT_val[1], GT_val[5]))
-                    diff1 = ((box1[0] - gt1[0]) ** 2 + (box1[1] - gt1[1]) ** 2) ** 0.5
-                    box2 = (max(int(box[0]), int(box[0] + box[2])), max(int(box[1]), int(box[1] + box[3])))
-                    gt2 = (max(GT_val[0], GT_val[4]), max(GT_val[1], GT_val[5]))
-                    diff2 = ((box2[0] - gt2[0]) ** 2 + (box2[1] - gt2[1]) ** 2) ** 0.5
-                    accuracy += diff1 + diff2
+                    # box1 = (min(int(box[0]), int(box[0] + box[2])), min(int(box[1]), int(box[1] + box[3])))
+                    # gt1 = (min(GT_val[0], GT_val[4]), min(GT_val[1], GT_val[5]))
+                    # diff1 = ((box1[0] - gt1[0]) ** 2 + (box1[1] - gt1[1]) ** 2) ** 0.5
+                    # box2 = (max(int(box[0]), int(box[0] + box[2])), max(int(box[1]), int(box[1] + box[3])))
+                    # gt2 = (max(GT_val[0], GT_val[4]), max(GT_val[1], GT_val[5]))
+                    # diff2 = ((box2[0] - gt2[0]) ** 2 + (box2[1] - gt2[1]) ** 2) ** 0.5
+                    # accuracy += diff1 + diff2
 
                     cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[0] + box[2]), int(box[1] + box[3])),
                                   (255, 0, 0), 2)
@@ -90,16 +91,16 @@ def track(tracker_model, tracker_name):
 
     print(tracker_name)
     print('Miss: ', miss)
-    print('Accuracy: ', accuracy)
+    # print('Accuracy: ', accuracy)
     print()
 
     # cv2.waitKey(0)
 
 
-# track(cv2.TrackerBoosting_create, 'BOOSTING')
-# track(cv2.TrackerMIL_create, 'MIL')
-track(cv2.TrackerKCF_create, 'KCF')
-track(cv2.TrackerTLD_create, 'TLD')
-track(cv2.TrackerMedianFlow_create, 'MEDIANFLOW')
-track(cv2.TrackerCSRT_create, 'CSRT')
+# track(cv2.TrackerBoosting_create, 'BOOSTING')    # 0
+# track(cv2.TrackerMIL_create, 'MIL')    # 0
+# track(cv2.TrackerKCF_create, 'KCF')    # 1074
+# track(cv2.TrackerTLD_create, 'TLD')    # 34
+# track(cv2.TrackerMedianFlow_create, 'MEDIANFLOW')    # 113
+# track(cv2.TrackerCSRT_create, 'CSRT')    # 16
 track(cv2.TrackerMOSSE_create, 'MOSSE')
