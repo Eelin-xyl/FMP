eb5263c63781f9964192dd6502eccc8b373c7980
BOSCH Eelin <xyl-eelin@qq.com>
2021-11-15 17:28:14 +0800

    office1115

--- testmultiprocess.py
+++ testmultiprocess.py
@@ -1,15 +1,17 @@
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Pipe
 import queue
 import time
 
 
-def worker(interval, queue):
+def worker(interval, queue, recv_port):
     n = 5
     while n > 0:
-        t, a = queue.get()
-        print(t)
-        print(a)
+        # t, a = queue.get()
+        r, c = recv_port.recv()
+        # print(t)
+        # print(a)
         print("The time is {0}".format(time.ctime()))
+        print(r, c)
         time.sleep(interval)
         n -= 1
 
@@ -18,11 +20,13 @@ if __name__ == "__main__":
 
     queue = Queue()
     queue.put(['test1', '1'])
-    # send_port.send('test2')
-    # send_port.send('test3')
+    send_port, recv_port = Pipe()
+    send_port.send('test2')
+
     # send_port.send('test4')
     # send_port.send('test5')
-    p = Process(target=worker, args=(2, queue))
+    p = Process(target=worker, args=(2, queue, recv_port))
+    send_port.send('test3')
     # p.daemon = True
     p.start()
     print("p.pid:", p.pid)
